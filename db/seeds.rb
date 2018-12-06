c28b8a9f95a1c78df5c57404489e90f500fef8d6
natashabuck <natashabuck1@gmail.com>
2018-12-06 14:07:49 +0000

    added instructions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,22 @@
 ###############################################################
-# STEP 1: ADDING STORIES
+# STEP 1: SET UP ENVIRONMENT
+
+# To seed the db, you'll need to go through both parts below. When you're on Step 1, comment out Step 2, and vice versa. 
+
+# Every time you have a new shell session:
+    # go into the 'lib' folder and ctrl+click on the file 'NewsApp-7584f1ce17a2.json'
+    # select 'copy path'
+    # IN THE SHELL, paste the following: 
+
+    #   'export GOOGLE_APPLICATION_CREDENTIALS='[PATH]'
+
+    # for example:
+    # export GOOGLE_APPLICATION_CREDENTIALS='/Users/natashabuck/dev/mod3/news-app-rails/lib/NewsApp-7584f1ce17a2.json'
+
+# Part 3(sentiment) should take about 10-15 minutes total.  
+
+###############################################################
+# STEP 2: ADDING STORIES
 
 # Story.delete_all
 # Country.delete_all
@@ -15,7 +32,7 @@
 # end
 
 ###############################################################
-# STEP 2: ADDING SENTIMENT
+# STEP 3: ADDING SENTIMENT
 
 include Scraper
 
@@ -28,131 +45,3 @@ Story.where("sentiment = 0").each do |story|
     story.update(sentiment: Scraper.get_page(story.url))
     story.save
 end
-
-###############################################################
-# # only seed country array once
-# array = %w(
-#     Argentina
-#         ar
-#     Australia
-#         au
-#     Austria
-#         at
-#     Belgium
-#         be
-#     Brazil
-#         br
-#     Bulgaria
-#         bg
-#     Canada
-#         ca
-#     China
-#         cn
-#     Colombia
-#         co
-#     Cuba
-#         cu
-#     Czech\ Republic
-#         cz
-#     Egypt
-#         eg
-#     France
-#         fr
-#     Germany
-#         de
-#     Greece
-#         gr
-#     Hong\ Kong
-#         hk
-#     Hungary
-#         hu
-#     India
-#         in
-#     Indonesia
-#         id
-#     Ireland
-#         ie
-#     Israel
-#         il
-#     Italy
-#         it
-#     Japan
-#         jp
-#     Latvia
-#         lv
-#     Lithuania
-#         lt
-#     Malaysia
-#         my
-#     Mexico
-#         mx
-#     Morocco
-#         ma
-#     Netherlands
-#         nl
-#     New\ Zealand
-#         nz
-#     Nigeria
-#         ng
-#     Norway
-#         no
-#     Philippines
-#         ph
-#     Poland
-#         pl
-#     Portugal
-#         pt
-#     Romania
-#         ro
-#     Russia
-#         ru
-#     Saudi\ Arabia
-#         sa
-#     Serbia
-#         rs
-#     Singapore
-#         sg
-#     Slovakia
-#         sk
-#     Slovenia
-#         si
-#     South\ Africa
-#         za
-#     South\ Korea
-#         kr
-#     Sweden
-#         se
-#     Switzerland
-#         ch
-#     Taiwan
-#         tw
-#     Thailand
-#         th
-#     Turkey
-#         tr
-#     UAE
-#         ae
-#     Ukraine
-#         ua
-#     United\ Kingdom
-#         gb
-#     United\ States\ of\ America
-#         us
-#     Venuzuela
-#         ve)
-
-# country = []
-# key = []
-
-# array.each do |k|
-#     k.length < 3 ? key.push(k) : country.push(k)
-
-# end
-
-# hash = {}
-# n = 0  
-# country.each do |c|   
-     
-#         hash[c] = key[n]
-#         n+=1
-# end
